05acf933eda4bfad52440763642a780b011b2ae1
LAB11_USER ON WS29 <L11X09W29@LabHCI.net>
2026-09-18 09:52:14 +0800

    Added another change in Clone A - Raymond.Rasonabe

--- orders.js
+++ orders.js
@@ -19,6 +19,7 @@ function calculateLoyaltyPoints(order) {
   const e = order.total / 10;
   if(order > 100) return e * 1.5;
   return Math.floor(e);
+  //e
 }
 
 module.exports = { createOrder, applyDiscount, cancelOrder, calculateLoyaltyPoints };
